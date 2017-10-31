42650cd76377bd67853638621e2562e21c654f45
Daryl Lozupone <daryl@newclarity.net>
2017-10-31 17:10:14 -0400

    Implement Source_Model::_call()

--- includes/class-source-model.php
+++ includes/class-source-model.php
@@ -20,4 +20,41 @@ class Source_Model extends \WPLib_Model_Base {
      */
     protected $_source_object;
 
+    /**
+     * @param  string $method_name
+     * @param  array $args
+     * @return mixed|null
+     */
+    function __call( $method_name, $args = array() ) {
+
+        do {
+
+            $property = "_{$method_name}";
+
+            if ( property_exists( __CLASS__, $method_name ) ) {
+                $value = $this->{$property};
+                break;
+            }
+
+            if ( isset( $this->source_object()->{$method_name} ) ) {
+                $value = $this->source_object()->{$method_name};
+                break;
+            }
+            
+            $value = null;
+        } while ( false );
+
+        return $value;
+
+    }
+
+    /**
+     * @return \stdClass
+     */
+    protected function source_object() {
+
+        return $this->_source_object;
+
+    }
+
 }
