9ce47415eccf8adf4f4bc9293f62fe5c8abbdafc
Daryl Lozupone <daryl@newclarity.net>
2017-11-07 16:20:00 -0500

    Refactor Post_View::the_image_url() for simplicity

--- includes/class-post-view.php
+++ includes/class-post-view.php
@@ -26,86 +26,14 @@ class Post_View extends \WPLib_View_Base {
                 break;
             }
 
-            $url = $this->model()->image();
-
             // Do we need to resize it?
             if ( is_null( $maxh ) && is_null( $maxw ) ) {
                 break;
             }
 
-            // generate a unique filename for this image with the specified dimensions
-            $fileargs = array(
-                basename( $url ),
-                $maxh,
-                $maxw,
-                $crop,
-            );
+            $image = self::_resize_image( $this->model()->image(), $maxw, $maxh, $crop );
 
-            $filename = md5( implode( ',', $fileargs ) );
-
-            $path = WP_CONTENT_DIR . '/uploads/juicer/cache/' . $filename;
-
-            // get the file extension
-            $extension = '';
-            if ( preg_match( '#^http[s]{0,1}://.*?\.([a-zA-Z0-9]{3,4})$#', $url, $matches ) ) {
-                $extension = $matches[1];
-            }
-
-            // Does the file already exist?
-            if ( file_exists( $path . '.' . $extension ) ) {
-                $url = sprintf( '%1$s/uploads/juicer/cache/%2$s.%3$s', WP_CONTENT_URL, $filename, $extension );
-                break;
-            }
-
-            $response = wp_remote_get( $url );
-
-            if ( $response instanceof \WP_Error){
-                break;
-            }
-
-            // Was the download successful?
-            if ( ! ( 200 === wp_remote_retrieve_response_code( $response ) ) ) {
-                break;
-            }
-
-            // store it temporarily
-            $tmpfile = sprintf( '/tmp/%1$s', md5( $url ) );
-
-            if ( ! file_put_contents( $tmpfile, wp_remote_retrieve_body( $response ) ) ) {
-                break;
-            }
-
-            // Get an image editor object
-            $editor = wp_get_image_editor( $tmpfile );
-
-            // can we edit this image file?
-            if (  $editor instanceof \WP_Error) {
-                break;
-            }
-
-            // then resize to the dimensions specified
-            $result = $editor->resize( $maxw, $maxh, $crop );
-
-
-            if ( ! $result || $result instanceof \WP_Error ) {
-                break;
-            }
-
-            // make sure we have a cache directory
-            if ( ! is_dir( WP_CONTENT_DIR . '/uploads/juicer/cache/' ) ) {
-                if ( ! mkdir( WP_CONTENT_DIR . '/uploads/juicer/cache/', 0775, true  ) ) {
-                    break;
-                }
-            }
-
-            // and then save the modified image
-            $result = $editor->save( $path );
-
-            if ( $result instanceof \WP_Error) {
-                break;
-            }
-
-            $url = WP_CONTENT_URL . '/uploads/juicer/cache/' . $result['file'];
+            $url = str_replace( WP_CONTENT_DIR, WP_CONTENT_URL, $image );
 
         } while ( false );
 
@@ -125,6 +53,115 @@ class Post_View extends \WPLib_View_Base {
     function the_timestamp( $format = 'M d, Y' ) {
 
         echo date( $format, strtotime( $this->model()->timestamp() ) );
+
+    }
+
+	/**
+	 * @param  string $url
+	 * @param  null   $width
+	 * @param  null   $height
+	 * @return string the file path
+	 */
+    private function _resize_image( $url, $width = null, $height = null, $crop = false ) {
+
+    	$path = '';
+
+	    // generate a unique filename for this image with the specified dimensions
+	    $fileargs = array(
+		    basename( $url ),
+		    $width,
+		    $height,
+		    $crop,
+	    );
+
+	    $filename = md5( implode( ',', $fileargs ) );
+
+	    $path = WP_CONTENT_DIR . '/uploads/juicer/cache/' . $filename;
+
+	    do {
+		    // get the file extension
+		    $extension = '';
+		    if ( preg_match( '#^http[s]{0,1}://.*?\.([a-zA-Z0-9]{3,4})$#', $url, $matches ) ) {
+			    $extension = $matches[1];
+		    }
+
+		    // Does the file already exist?
+		    if ( file_exists( $maybe = $path . '.' . $extension ) ) {
+		    	$path = $maybe;
+			    break;
+		    }
+
+		    $tmpfile = self::_download_image( $url );
+
+		    // Get an image editor object
+		    $editor = wp_get_image_editor( $tmpfile );
+
+		    // can we edit this image file?
+		    if (  $editor instanceof \WP_Error) {
+			    break;
+		    }
+
+		    // then resize to the dimensions specified
+		    $result = $editor->resize( $width, $height, $crop );
+
+
+		    if ( ! $result || $result instanceof \WP_Error ) {
+			    break;
+		    }
+
+		    // make sure we have a cache directory
+		    if ( ! is_dir( WP_CONTENT_DIR . '/uploads/juicer/cache/' ) ) {
+			    if ( ! mkdir( WP_CONTENT_DIR . '/uploads/juicer/cache/', 0775, true  ) ) {
+				    break;
+			    }
+		    }
+
+		    // and then save the modified image
+		    $result = $editor->save( $path );
+
+		    if ( $result instanceof \WP_Error) {
+			    break;
+		    }
+
+		    $path = $result['path'];
+	    } while ( false );
+
+	    return $path;
+
+    }
+
+	/**
+	 * @param  string $url
+	 *
+	 * @return string The location of the downloaded image file
+	 */
+    private function _download_image( $url ) {
+
+    	$filepath = '';
+
+	    do {
+		    $response = wp_remote_get( $url );
+
+		    if ( $response instanceof \WP_Error){
+			    break;
+		    }
+
+		    // Was the download successful?
+		    if ( ! ( 200 === wp_remote_retrieve_response_code( $response ) ) ) {
+			    break;
+		    }
+
+		    // store it temporarily
+		    $tmpfile = sprintf( '/tmp/%1$s', md5( $url ) );
+
+		    if ( ! file_put_contents( $tmpfile, wp_remote_retrieve_body( $response ) ) ) {
+			    break;
+		    }
+
+		    $filepath = $tmpfile;
+	    } while ( false );
+
+	    return $filepath;
 
     }
 
